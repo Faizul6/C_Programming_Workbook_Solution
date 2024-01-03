c186e9284a96a8a2e984494075126471546feace
Faizul Robin <126159698+Faizul6@users.noreply.github.com>
2024-01-03 22:13:37 +0600

    Add files via upload

---
+++ Exercise_22.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    printf("Letter  Upper   Small\n");
+    for(int i=0;i<=25;i++)
+    {
+        printf("%c       ",i+65);
+        printf(" %d      ",i+65);
+        printf("%d\n",i+97);
+    }
+
+
+
+
+
+
+}
